1c96dc4178b5dbd2d304729236717417d0b8f2e1
AJ Muir <andrew.muir.22@cnu.edu>
2024-11-09 14:00:09 -0500

    needed to commit oops

---
+++ invite.php
@@ -0,0 +1,25 @@
+<?php
+include 'conn.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    // Retrieve form data
+    $user = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+}
+
+// Prepare and bind
+$stmt = $conn->prepare("INSERT INTO friends (name, email, phone) VALUES (?, ?, ?)");
+$stmt->bind_param("ssss", $user, $email, $phone);
+
+// Execute the query
+if ($stmt->execute()) {
+    header("Location: welcome.html")
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+// Close connection
+$stmt->close();
+$conn->close();
+?>
